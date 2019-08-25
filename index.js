475c5e410a032497a9979860725ae3d4bf61a783
shibasis <shibasis0801@gmail.com>
2019-08-25 23:34:11 +0530

    Added Express and creating Hello World API

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const express = require('express');
+
+const server = express()
+
 console.log("Started Pravega Backend");
+
+const port = 1234
+server.listen(port, (args) => {
+    if (args) 
+        console.log(args);
+    
+    console.log(`Started HTTP server at http://localhost:${port}`);
+})
+
+server.get('/', (err, res) => {
+    res.send({
+        message: "Hello Motu"
+    })
+});
