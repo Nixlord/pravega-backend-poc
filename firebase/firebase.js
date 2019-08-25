fe04c0d887797af398d421f60313af9329f1c4af
shibasis <shibasis0801@gmail.com>
2019-08-26 00:51:54 +0530

    Connected Firebase and Heroku together using serviceAccount and API key

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,9 +1,12 @@
 // This is for Prod, there can be another similar file for Dev environment with a different Firebase project (Ex : Staging)
 const admin = require("firebase-admin");
 const process = require('process');
+const fs = require('fs');
 
-const credential = require('./pravegaherokufirebase-firebase-adminsdk-0a1ch-9727ab5137.json') ||  process.env.FIREBASE_SERVICE_ACCOUNT;
-console.log(credential);
+const credentialFile = './pravegaherokufirebase-firebase-adminsdk-0a1ch-9727ab5137.json';
+
+const credential = fs.existsSync(credentialFile) ? require(credentialFile) : process.env.FIREBASE_SERVICE_ACCOUNT;
+// console.log(credential);
 
 admin.initializeApp({
   credential: admin.credential.cert(credential),
