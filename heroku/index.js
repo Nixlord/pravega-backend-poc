b1d4e05d695a5166591fa2e0bbcf31a14ca7d8a2
shibasis <shibasis0801@gmail.com>
2019-08-27 23:43:12 +0530

    Trying out Heroku postgres

--- heroku/index.js
+++ heroku/index.js
@@ -1,10 +1,14 @@
 const firebase = require('./firebase/firebase');
 const db = firebase.database();
     
+const postgres = require('./postgres');
+postgres.connect();
+
 const express = require('express');
 const process = require('process');
 
 const server = express()
+
 
 console.log("Started Pravega Backend");
 
@@ -17,10 +21,21 @@ server.listen(port, (args) => {
 })
 
 server.get('/', (err, res) => {
-    res.send({
-        message: "Hello Motu"
-    })
     db.ref('/message').set({
         message: "Hello Motu"
     })
+
+    postgres.query('SELECT * FROM friends', (err, result) => {  
+        if (err) throw err;
+        
+        res.send(result);
+
+        // for (let row of res.rows) {
+        //     console.log(row);
+        //     console.log(JSON.stringify(row));
+        // }
+        
+        postgres.end();
+    });
 });
+
---
+++ heroku/postgres/database.js
@@ -0,0 +1,15 @@
+const postgres = require('./postgres');
+
+
+postgres.connect();
+
+postgres.query('SELECT * FROM friends', (err, res) => {  
+  if (err) throw err;
+  
+  for (let row of res.rows) {
+      console.log(row);
+      console.log(JSON.stringify(row));
+  }
+
+  postgres.end();
+});
