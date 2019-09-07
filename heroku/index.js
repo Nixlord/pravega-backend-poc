9c881fc4723b812c371bc9a102b4173ad7d8ccbd
shibasis <shibasis0801@gmail.com>
2019-09-07 19:52:10 +0530

    Added Firebase Functions

--- heroku/index.js
+++ heroku/index.js
@@ -1,18 +1,29 @@
 const firebase = require('./firebase/firebase');
 const db = firebase.database();
-    
+//ig you read, then read this now
+//if you finish that, and iM still talking, then read firebase-admin.
+//if that is also done, read postgres, heroku postgres.    
 const postgres = require('./postgres/postgres');
 postgres.connect();
+
+/**
+Use firebase admin to change a value in realtime database.
+OnChange in realtime database, trigger a cloud function.
+From that function change a value in realtime database.
+From server side listeners, get that change.
+
+USE a node in realtime database to communicate changes from and to heroku.
+*/
 
 const express = require('express');
 const process = require('process');
 
 const server = express()
 
-
 console.log("Started Pravega Backend");
 
 const port = process.env.PORT || 8000;
+
 server.listen(port, (args) => {
     if (args) 
         console.log(args);
@@ -20,7 +31,13 @@ server.listen(port, (args) => {
     console.log(`Started HTTP server at http://localhost:${port}`);
 })
 
-server.get('/', (err, res) => {
+server.get('/', (req, res) => {
+    res.send({
+        name: "Shibasis Patnaik"
+    })
+})
+
+server.get('/postgres', (req, res) => {
     db.ref('/message').set({
         message: "Hello Motu"
     })
@@ -37,5 +54,15 @@ server.get('/', (err, res) => {
         
         postgres.end();
     });
+});
+
+
+
+server.get('/firebase/:key/:value', (req, res) => {
+    const { key, value } = req.params;
+    const response = {}
+    response[key] = value;
+
+    res.send(response);
 });
 
